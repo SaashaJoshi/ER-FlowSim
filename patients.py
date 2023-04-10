8b8cbff2be593a51906ba7fac889fa2c305966f3
SaashaJoshi <saashajoshi08@gmail.com>
2023-04-09 17:24:34 -0700

    code cleaning

--- patients.py
+++ patients.py
@@ -9,8 +9,7 @@ class Patient:
 
     patient_count = 0
 
-    def __init__(self, arrival_time,
-                 leave_time=0, ctas_level=None,
+    def __init__(self, arrival_time, leave_time=0, ctas_level=None,
                  bed_assigned=None):
         self.env = simpy.Environment()
 
@@ -18,7 +17,6 @@ class Patient:
         self.id = Patient.patient_count
 
         self.arrival_time = arrival_time
-        # self.service_time = service_time
         self.triage_waiting_time = 0
         self.ed_waiting_time = 0
         self.medication_waiting_time = 0
@@ -26,21 +24,10 @@ class Patient:
         self.leave_time = leave_time
         self.ctas_level = ctas_level
         self.tests = []
-        # self.bed_assigned = bed_assigned
+        self.bed_assigned = bed_assigned
 
     def get_ctas_level(self):
         return int(random.randint(1, 5))
 
     def get_triage_treatment_review(self):
         return random.randint(0, 1)
-
-    # def get_test_result(self, test):
-    #     if test == 0:
-    #         # MRI
-    #         yield simpy.Timeout(self.env, 45)
-    #     elif test == 1:
-    #         # XRay
-    #         yield simpy.Timeout(self.env, 15)
-    #     elif test == 2:
-    #         # CT
-    #         yield simpy.Timeout(self.env, 30)
